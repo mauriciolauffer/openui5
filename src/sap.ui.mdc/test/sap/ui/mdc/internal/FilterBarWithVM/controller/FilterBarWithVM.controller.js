b61771b2fac2535acbcb9fd2c24e05358b3ffa83
Thomas Buechler <thomas.buechler@sap.com>
2025-08-06 11:39:23 +0200

    [INTERNAL] MDC: Provide internal filterbar sample using VM
    Change-Id: I1fdcff52f15115a48c631e763d015d41952551d8

---
+++ src/sap.ui.mdc/test/sap/ui/mdc/internal/FilterBarWithVM/controller/FilterBarWithVM.controller.js
@@ -0,0 +1,32 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter"
+], function(
+	Controller,
+	JSONModel,
+	formatter
+) {
+	"use strict";
+
+	return Controller.extend("mdc.sample.controller.FilterBarWithVM", {
+		formatter: formatter,
+		onInit: function() {
+			const oModel = new JSONModel({
+				conditionsText: "",
+				editorHeight: 400
+			});
+			this.getView().setModel(oModel);
+		},
+		onFiltersChanged: function(oEvent) {
+			const oModel = this.getView().getModel();
+			if (!oModel) {
+				return;
+			}
+
+			const oConditions = this.getView().byId("mountainsFilterbar").getConditions();
+			const sConditions = JSON.stringify(oConditions, "\t", 4);
+			oModel.setProperty("/conditionsText", sConditions);
+		}
+	});
+});
