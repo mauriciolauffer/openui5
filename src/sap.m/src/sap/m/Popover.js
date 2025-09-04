f92f3032e1de12e60930b25d66d8fd6d1d1d29ab
Lidiya Georgieva <lidiya.georgieva@sap.com>
2025-09-04 16:24:25 +0300

    [FIX] sap.m.Popover: Added proper aria-labelledby for custom headers
    When a custom header is present, two options are available:
    - Recommended: `sap.m.Title` is included in the custom header and its ID labels the Popover.
    - If there is no `sap.m.Title` in the custom header, the entire
    custom header's ID labels the Popover.
    
    SNOW: DINC0182688
    Change-Id: I319bd5b227cb904388ad57f911efd4ca9e9f7b67

--- src/sap.m/src/sap/m/Popover.js
+++ src/sap.m/src/sap/m/Popover.js
@@ -264,6 +264,7 @@ sap.ui.define([
 
 					/**
 					 * Any control that needed to be displayed in the header area. When this is set, the showHeader property is ignored, and only this customHeader is shown on the top of popover.
+					* <br/><b>Note:</b> To improve accessibility, titles with heading level <code>H1</code> should be used inside the custom header.
 					 */
 					customHeader: {type: "sap.ui.core.Control", multiple: false},
 
@@ -2693,6 +2694,12 @@ sap.ui.define([
 			}
 		};
 
+		Popover.prototype._getTitles = function (oContainer) {
+			return oContainer.findAggregatedObjects(true, function(oObject) {
+				return oObject.isA("sap.m.Title");
+			});
+		};
+
 		/**
 		 * Provides the accessibility options of the control.
 		 *
@@ -2711,10 +2718,23 @@ sap.ui.define([
 				return mAccOptions;
 			}
 
-			var oHeaderId = oCustomHeader ? oCustomHeader.getId() : this.getHeaderTitle().getId();
+			if (oCustomHeader) {
+				// if there are titles in the header, add all of them to labels, else use the full header
+				var aTitles = this._getTitles(oHeader);
+
+				if (aTitles.length) {
+					aAriaLabels = aTitles.map(function (oTitle) {
+						return oTitle.getId();
+					});
+				} else {
+					aAriaLabels = oHeader.getId();
+				}
+			} else {
+				aAriaLabels = this.getHeaderTitle().getId();
+			}
 
 			// If we have a header/title, we add a reference to it in the beginning of the aria-labelledby attribute
-			aAriaLabels = Array.prototype.concat(oHeaderId, this.getAssociation("ariaLabelledBy", []));
+			aAriaLabels = Array.prototype.concat(aAriaLabels, this.getAssociation("ariaLabelledBy", []));
 			mAccOptions.labelledby = aAriaLabels.join(' ');
 
 			return mAccOptions;
