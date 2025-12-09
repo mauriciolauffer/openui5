a0dd70d07b4b5fcf9722b7045be598e5e4c4318c
i310817 <gergana.marcheva@sap.com>
2025-12-09 16:40:25 +0200

    [FIX] sap.m.ObjectStatus: state information is referenced in
    ariaLabelledBy
    
    SNOW: DINC0695045
    
    Change-Id: I4022b745c584cbece2cda3ab768c467135823d97

--- src/sap.m/test/sap/m/qunit/ObjectStatus.qunit.js
+++ src/sap.m/test/sap/m/qunit/ObjectStatus.qunit.js
@@ -720,7 +720,7 @@ sap.ui.define([
 
 		// Assert
 		assert.strictEqual(oObjectStatus.getDomRef().getAttribute("aria-labelledby"),
-			"info oslab-title oslab-text oslab-statusIcon oslab-state",
+			"info oslab-title oslab-text oslab-statusIcon oslab-state-text",
 			"ObjecStatus's content information is added in aria-labelledby alongside the label");
 
 		// Cleanup
